fb21985ae5b96e7d55126d58e8a2684f598e5a48
Sena_Catana <sena@magnoliagames.co>
2022-08-01 11:22:47 +0300

    initial commit

---
+++ Assets/Scripts/SelectLevelOpener.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevelOpener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
