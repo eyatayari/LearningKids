39f3d655ff962d6d9948429b169dd838f19aea50
Sena_Catana <sena@magnoliagames.co>
2022-08-02 17:39:08 +0300

    design health coins timer bar

---
+++ Assets/Scripts/health.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Health;
+    public int nbrHeart;
+}
